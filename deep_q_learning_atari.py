d271b9e6d38076eece2b5dfc58d72f8bf407b9c5
grifguitar <xlootin@niuitmo.ru>
2021-06-17 00:07:45 +0300

    init atari

--- deep_q_learning_atari.py
+++ deep_q_learning_atari.py
@@ -1,18 +1,77 @@
 import gym
 import tensorflow as tf
+import collections as cll
 import numpy as np
+import random as rnd
 import matplotlib.pyplot as plt
 
-from deep_q_learning import Agent
+
+class Agent:
+    def _build_model(self):
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.InputLayer(input_shape=self.observation_shape))
+        model.add(tf.keras.layers.Flatten())
+        model.add(tf.keras.layers.Dense(32, activation='relu'))
+        model.add(tf.keras.layers.Dense(32, activation='relu'))
+        model.add(tf.keras.layers.Dense(self.action_size, activation='linear'))
+        model.compile(optimizer=tf.keras.optimizers.Adam(),
+                      loss='mse')
+        return model
+
+    def __init__(self, env, max_total_step_cnt, gamma=0.6, max_epsilon=0.3, min_epsilon=0.01):
+        self.env = env
+
+        self.observation_shape = env.observation_space.shape
+        self.action_size = env.action_space.n
+
+        self.buffer = cll.deque(maxlen=10000)
+
+        self.gamma = gamma
+        self.max_epsilon = max_epsilon
+        self.min_epsilon = min_epsilon
+        self.stride_length = (self.max_epsilon - self.min_epsilon) / max_total_step_cnt
+        self.epsilon = self.max_epsilon
+
+        self.q_network = self._build_model()
+        self.target_network = self._build_model()
+        self.copy_weights()
+
+    def copy_weights(self):
+        self.target_network.set_weights(self.q_network.get_weights())
+
+    def save_to_buffer(self, state, action, reward, next_state, terminated):
+        self.buffer.append((state, action, reward, next_state, terminated))
+
+    @staticmethod
+    def get_best_action(network, state):
+        predictions = network.predict(state)
+        return np.argmax(predictions[0])
+
+    @staticmethod
+    def get_best_value(network, state):
+        predictions = network.predict(state)
+        return np.amax(predictions[0])
+
+    def get_action(self, state):
+        self.epsilon -= self.stride_length
+        if np.random.rand() <= self.epsilon:
+            return self.env.action_space.sample()
+        else:
+            return self.get_best_action(self.q_network, state)
+
+    def retrain(self, batch_size):
+        batch = rnd.sample(self.buffer, batch_size)
+        for state, action, reward, next_state, terminated in batch:
+            predictions = self.q_network.predict(state)
+            if terminated:
+                predictions[0][action] = reward
+            else:
+                predictions[0][action] = reward + self.gamma * Agent.get_best_value(self.target_network, next_state)
+            self.q_network.fit(state, predictions, epochs=1, verbose=0)
 
 
 def draw(env, i_episode, i_step, state, reward, eps):
-    img = env.render(mode='rgb_array')
-    plt.title('frame_{x}_{y}.png'.format_map({'x': i_episode, 'y': i_step}))
-    plt.axis('off')
-    plt.imshow(img)
-    plt.savefig('new_images/frame_{x}_{y}.png'.format_map({'x': i_episode, 'y': i_step}))
-    # plt.show()
+    # env.render()
     print(i_episode, i_step, state, reward, eps)
 
 
@@ -23,23 +82,12 @@ def solve():
     print('action_space: ', env.action_space, env.action_space.n)
     print('------------------------------')
 
-    episodes_count = 2
+    episodes_count = 50
     max_steps_count = 200
+    batch_size = 20
+    copy_period = 10
 
     agent = Agent(env, episodes_count * max_steps_count)
-
-    # load json and create model
-    json_file = open('model1.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    agent.q_network = tf.keras.models.model_from_json(loaded_model_json)
-    agent.target_network = tf.keras.models.model_from_json(loaded_model_json)
-    # load weights into new model
-    agent.q_network.load_weights("model1.h5")
-    agent.target_network.load_weights("model1.h5")
-    print("Loaded model from disk")
-
-    agent.copy_weights()
     agent.q_network.summary()
 
     total_rewards = list()
@@ -57,7 +105,7 @@ def solve():
         for i_step in range(max_steps_count):
 
             # выбираем оптимальное действие из текущего состояния
-            action = Agent.get_best_action(agent.q_network, state)
+            action = agent.get_action(state)
 
             # делаем действие и получаем результаты
             next_state, reward, done, info = env.step(action)
@@ -67,10 +115,21 @@ def solve():
             # модифицируем награду
             reward += 300 * (abs(next_state[0][1]) - abs(state[0][1]))
 
+            # сохраняем опыт
+            agent.save_to_buffer(state, action, reward, next_state, done)
+
+            # обучаем q_network на случайной выборке размера batch_size из нашего опыта
+            if len(agent.buffer) > batch_size:
+                agent.retrain(batch_size)
+
             # если игра закончилась, выходим
             if done:
                 print("Episode finished after {} timesteps".format(i_step + 1), total_reward)
                 break
+
+            # копируем веса с интервалом в copy_period шагов
+            if i_step % copy_period == 0:
+                agent.copy_weights()
 
             # рисуем текущее состояние среды
             draw(env, i_episode, i_step, state, reward, agent.epsilon)
@@ -78,9 +137,10 @@ def solve():
             # переходим в новое состояние
             state = next_state
 
+        agent.copy_weights()
+
         total_rewards.append(total_reward)
 
-    plt.clf()
     plt.title("total_rewards")
     plt.plot(total_rewards)
     plt.show()
@@ -88,6 +148,16 @@ def solve():
     plt.title("total_qs")
     plt.plot(total_qs)
     plt.show()
+
+    model = agent.q_network
+
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("new_model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("new_model.h5")
+    print("Saved model to disk")
 
     env.close()
 
